ee479d0546bd40f2b8b55e1740f001d12622820d
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-12 14:48:40 +0530

    Update views.py

--- grammar_correct/views.py
+++ grammar_correct/views.py
@@ -19,7 +19,10 @@ def grammar_checker(request):
             'corrections': [{'ruleId': match.ruleId, 'message': match.message, 'replacements': match.replacements} for match in matches],
             'corrected_text': corrected_text,
         }
+        user_id = request.session.get('user_id')
 
+        gram_check = GrammarCheck(user1=user_id, input_text=input_text, corrected_text=corrected_text)
+        gram_check.save()
         # return JsonResponse(response_data)
         return render(request,'grammar.html',{'result':corrected_text,'input':input_text})
 
