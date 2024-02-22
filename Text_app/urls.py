bd19c65edb518be761ec708e673a7e21cea18953
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-22 11:47:27 +0530

    Update urls.py

--- Text_app/urls.py
+++ Text_app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pdf/',include('pdf_to_summary.urls')),
-    path('SummarEase/', include('login.urls')),
+    path('', include('login.urls')),
     path('Text/',include('text.urls')),
     path('Grammar_Check/',include('grammar_correct.urls')),
     path('history/', include('history.urls')),
