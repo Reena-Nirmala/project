19b71ec9165af3229ab59862ebe127d408aeb6dd
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-25 16:04:55 +0530

    Update models.py

--- pdf_to_summary/models.py
+++ pdf_to_summary/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class PdfDocument(models.Model):
     pdf_file = models.FileField(upload_to='pdfs/')
-    pages_to_summarize = models.IntegerField()
+    # pages_to_summarize = models.IntegerField()
+    pages_to_summarize = models.CharField(max_length=20)  # Change the field type to CharField
+
     summarized_text = models.TextField(blank=True, null=True)
 
     def __str__(self):
