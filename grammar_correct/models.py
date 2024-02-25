64a25adbe327a419065b4612b9de87a3c7ddf733
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-25 16:43:10 +0530

    Update models.py

--- grammar_correct/models.py
+++ grammar_correct/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 # from django.contrib.auth.models import User
 from login.models import User
-# from django.utils import timezone
+from django.utils import timezone
 
 
 class GrammarCheck(models.Model):
