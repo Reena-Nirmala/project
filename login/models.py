881eb9a69da3f07b93c4f43aff0f00134554d907
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-25 16:28:57 +0530

    Update models.py

--- login/models.py
+++ login/models.py
@@ -27,8 +27,8 @@ class User(AbstractUser):
 
     objects = UserManager()
 
-    def set_password(self, raw_password):
-        self.password = raw_password  # Override to store password in plain text
+    # def set_password(self, raw_password):
+    #     self.password = raw_password  # Override to store password in plain text
 
     def __str__(self):
         return self.username
@@ -36,45 +36,3 @@ class User(AbstractUser):
 
 
 
-# myapp/models.py
-
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-#
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, username, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, username=username, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_superuser(self, email, username, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, username, password, **extra_fields)
-#
-# class User(AbstractBaseUser):
-#     email = models.EmailField(unique=True)
-#     username = models.CharField(max_length=30, unique=True)
-#     password = models.CharField(max_length=128)
-#     reset_password_token = models.CharField(max_length=32, blank=True, null=True)
-#
-#     # Add any additional fields you need for your user model
-#     # For example:
-#     # full_name = models.CharField(max_length=100)
-#     # is_active = models.BooleanField(default=True)
-#     # ...
-#
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#
-#     objects = CustomUserManager()
-#
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['username']
-#
-#     def __str__(self):
-#         return self.email
