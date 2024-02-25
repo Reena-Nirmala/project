4e8a3ff5859529a98c8af2986aa5664016300622
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-25 16:30:44 +0530

    Update models.py

--- pdf_to_summary/models.py
+++ pdf_to_summary/models.py
@@ -8,6 +8,8 @@ class PdfDocument(models.Model):
     pages_to_summarize = models.CharField(max_length=20)  # Change the field type to CharField
 
     summarized_text = models.TextField(blank=True, null=True)
+    user1 = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
         return f"{self.pdf_file.name} - Pages: {self.pages_to_summarize}"
