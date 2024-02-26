3bb35e30f6281746fe1537e4593c4885efaec96c
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-26 12:24:02 +0530

    Update models.py

--- text/models.py
+++ text/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+import pytz
 
 class TextSummary(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,6 +12,10 @@ class TextSummary(models.Model):
     user1 = models.IntegerField(default=0)
 
     created_at = models.DateTimeField(default=timezone.now, editable=False)
+    def save(self, *args, **kwargs):
+        # Set created_at to Indian Standard Time (IST)
+        self.created_at = timezone.now().astimezone(pytz.timezone('Asia/Kolkata'))
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Summary for {self.user1}'
